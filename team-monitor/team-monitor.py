7c25d54ea8fbbdba3111678b1231284da0842ed4
Paul Tagliamonte <paultag@ubuntu.com>
2010-07-27 20:21:31 -0400

    counting

--- team-monitor/team-monitor.py
+++ team-monitor/team-monitor.py
@@ -40,16 +40,22 @@ global_teams   = {}
 all_teams = launchpad.people[team_all]
 all_members = all_teams.members_details
 
+global_count = 0
+
 for member in all_members:
 	global_teams[member.member.name] = member.member.display_name
+	global_count = global_count + 1
 
 app_teams   = launchpad.people[team_app]
 app_members = app_teams.members_details
 
+app_count    = 0
+
 for member in app_members:
 	approved_teams[member.member.name] = member.member.display_name
+	app_count = app_count + 1
 
-print "Approved Teams: "
+print "Approved Teams ( " + str(app_count) + " ): "
 for team in approved_teams:
 	try:
 		print team + " ( " + approved_teams[team] + " ) "
@@ -57,7 +63,10 @@ for team in approved_teams:
 		print team + " ( Non ascii display name ) "
 
 print ""
-print "Unapproved Teams"
+
+global_count = global_count - app_count
+
+print "Unapproved Teams ( " + str(global_count) + " )"
 for team in global_teams:
 	try:
 		trash = approved_teams[team]
